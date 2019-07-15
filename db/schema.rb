d9b913e4120176663ca3d4b90f134cfe3b46c803
Brennan-Richards <brennanr0210@gmail.com>
2019-07-15 19:13:18 -0400

    Added users model. Deleted create-users branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_174756) do
+ActiveRecord::Schema.define(version: 2019_07_15_225056) do
 
   create_table "car_expenses", force: :cascade do |t|
   end
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 2019_07_15_174756) do
     t.decimal "medicare", precision: 11, scale: 2
     t.decimal "disability", precision: 11, scale: 2
     t.decimal "socialsecurity", precision: 11, scale: 2
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
